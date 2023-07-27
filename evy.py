527f52d81c00cab942d352daedb694c99a798f63
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-07-27 19:40:41 +0200

    Find tracks between 120 and 125 BPM

--- evy.py
+++ evy.py
@@ -8,13 +8,18 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 results = sp.current_user_saved_tracks(limit=50)
 
 saved_track_ids = []
+names = []
 
 for idx, item in enumerate(results["items"]):
     track = item["track"]
     track_id = track["id"]
+    name = track["name"]
 
     saved_track_ids.append(track_id)
+    names.append(name)
 
 features = sp.audio_features(tracks=saved_track_ids)
 
-print(features)
+for idx, track in enumerate(features):
+    if track["tempo"] >= 120 and track["tempo"] <= 125:
+        print(names[idx])

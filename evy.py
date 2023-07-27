cc34131c517ae9411ab38ae40e129835c813319c
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-07-27 20:02:26 +0200

    Support more tracks from the library

--- evy.py
+++ evy.py
@@ -1,25 +1,55 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
+MAX_LOOPS = 10
+
 scope = "user-library-read"
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-results = sp.current_user_saved_tracks(limit=50)
+# https://stackoverflow.com/a/312464
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
 
-saved_track_ids = []
-names = []
+def extract_info(results):
+    saved_track_ids = []
+    names = []
 
-for idx, item in enumerate(results["items"]):
-    track = item["track"]
-    track_id = track["id"]
-    name = track["name"]
+    for idx, item in enumerate(results["items"]):
+        track = item["track"]
 
-    saved_track_ids.append(track_id)
-    names.append(name)
+        saved_track_ids.append(track["id"])
+        names.append(track["name"])
 
-features = sp.audio_features(tracks=saved_track_ids)
+    return saved_track_ids, names
 
-for idx, track in enumerate(features):
-    if track["tempo"] >= 120 and track["tempo"] <= 125:
-        print(names[idx])
+def get_user_library():
+    results = sp.current_user_saved_tracks(limit=50)
+
+    saved_track_ids, names = extract_info(results)
+
+    loops = 0
+    while results['next']:
+        results = sp.next(results)
+
+        saved_track_ids_inner, names_inner = extract_info(results)
+        saved_track_ids += saved_track_ids_inner
+        names += names_inner
+
+        loops += 1
+
+        if loops >= MAX_LOOPS:
+            return saved_track_ids, names
+            
+saved_track_ids, names = get_user_library()
+
+saved_track_ids_chunks = chunks(saved_track_ids, 50)
+
+for outer_idx, saved_track_ids in enumerate(saved_track_ids_chunks):
+    features = sp.audio_features(tracks=saved_track_ids)
+
+    for idx, track in enumerate(features):
+        if track["tempo"] >= 120 and track["tempo"] <= 125:
+            print(names[(outer_idx * 50) + idx])
